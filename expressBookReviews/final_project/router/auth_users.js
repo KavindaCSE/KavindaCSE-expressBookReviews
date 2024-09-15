d6c319b2af9a72790b1e16e3c3875ecd0dcf4441
Kavinda <kavindasamarawickkrama2001@gmail.com>
2024-09-15 11:40:58 +0530

    first commit

---
+++ expressBookReviews/final_project/router/auth_users.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (user)=>{ //returns boolean
+  let filtered_users = users.filter((user)=> user.username === user);
+  if(filtered_users){
+      return true;
+  }
+  return false;
+}
+const authenticatedUser = (username,password)=>{ //returns boolean
+  if(isValid(username)){
+      let filtered_users = users.filter((user)=> (user.username===username)&&(user.password===password));
+      if(filtered_users){
+          return true;
+      }
+      return false;
+     
+  }
+  return false;
+  
+
+}
+
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  let user = req.body.username;
+  let pass = req.body.password;
+  if(!authenticatedUser(user,pass)){
+      return res.status(403).json({message:"User not authenticated"})
+  }
+
+  let accessToken = jwt.sign({
+      data: user
+  },'access',{expiresIn:60*60})
+  req.session.authorization = {
+      accessToken
+  }
+  res.send("User logged in Successfully")
+
+});
+
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  let userd = req.session.username;
+  let ISBN = req.params.isbn;
+  let details = req.query.review;
+  let rev = {user:userd,review:details}
+  books[ISBN].reviews = rev;
+  return res.status(201).json({message:`The review for the book with ISBN ${ISBN} has been updated/added`})
+  
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  let ISBN = req.params.isbn;
+  books[ISBN].reviews = {}
+  return res.status(200).json({messsage:"Review has been deleted"})
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
